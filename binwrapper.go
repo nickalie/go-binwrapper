0875af7fb427a6b6273255682719a08564802e1d
Nikolay Glushchenko <nick@nickalie.com>
2017-04-26 15:02:12 +0300

    added docs

--- binwrapper.go
+++ binwrapper.go
@@ -1,22 +1,23 @@
-// Binary wrapper that makes command line tools seamlessly available as local golang dependencies.
+// Package binwrapper provides executable wrapper that makes command line tools seamlessly available as local golang dependencies.
 // Inspired by and partially ported from npm package bin-wrapper: https://github.com/kevva/bin-wrapper
 package binwrapper
 
 import (
-	"path/filepath"
-	"os"
 	"errors"
-	"os/exec"
-	"net/url"
-	"strings"
-	"net/http"
-	"io"
-	"github.com/mholt/archiver"
 	"fmt"
+	"github.com/mholt/archiver"
+	"io"
 	"io/ioutil"
+	"net/http"
+	"net/url"
+	"os"
+	"os/exec"
+	"path/filepath"
 	"runtime"
+	"strings"
 )
 
+// Src defines executable source
 type Src struct {
 	url      string
 	os       string
@@ -24,6 +25,7 @@ type Src struct {
 	execPath string
 }
 
+// BinWrapper wraps executable and provides convenient methods to interact with
 type BinWrapper struct {
 	src      []*Src
 	dest     string
@@ -42,53 +44,53 @@ type BinWrapper struct {
 	debug bool
 }
 
-//Creates new Src instance
+// NewSrc creates new Src instance
 func NewSrc() *Src {
 	return &Src{}
 }
 
-//Sets a URL pointing to a file to download.
-func (s *Src) Url(value string) *Src {
+// URL sets a url pointing to a file to download.
+func (s *Src) URL(value string) *Src {
 	s.url = value
 	return s
 }
 
-//Tie the source to a specific OS. Possible values are same as runtime.GOOS
+// Os tie the source to a specific OS. Possible values are same as runtime.GOOS
 func (s *Src) Os(value string) *Src {
 	s.os = value
 	return s
 }
 
-//Tie the source to a specific arch. Possible values are same as runtime.GOARCH
+// Arch tie the source to a specific arch. Possible values are same as runtime.GOARCH
 func (s *Src) Arch(value string) *Src {
 	s.arch = value
 	return s
 }
 
-//Tie the src to a specific binary file
+// ExecPath tie the src to a specific binary file
 func (s *Src) ExecPath(value string) *Src {
 	s.execPath = value
 	return s
 }
 
-//Creates ready to use BinWrapper instance
+// NewBinWrapper creates BinWrapper instance
 func NewBinWrapper() *BinWrapper {
 	return &BinWrapper{}
 }
 
-//Adds a source to download
+// Src adds a Src to BinWrapper
 func (b *BinWrapper) Src(src *Src) *BinWrapper {
 	b.src = append(b.src, src)
 	return b
 }
 
-//Accepts a path which the files will be downloaded to
+// Dest accepts a path which the files will be downloaded to
 func (b *BinWrapper) Dest(dest string) *BinWrapper {
 	b.dest = dest
 	return b
 }
 
-//Define which file to use as the binary
+// ExecPath define a file to use as the binary
 func (b *BinWrapper) ExecPath(execPath string) *BinWrapper {
 
 	if b.autoExe && runtime.GOOS == "windows" {
@@ -103,43 +105,43 @@ func (b *BinWrapper) ExecPath(execPath string) *BinWrapper {
 	return b
 }
 
-//Adds .exe extension for windows executable path
+// AutoExe adds .exe extension for windows executable path
 func (b *BinWrapper) AutoExe() *BinWrapper {
 	b.autoExe = true
 	return b.ExecPath(b.execPath)
 }
 
-//Skips downloading a file
+// SkipDownload skips downloading a file
 func (b *BinWrapper) SkipDownload() *BinWrapper {
 	b.src = nil
 	return b
 }
 
-//Strips a number of leading paths from file names on extraction.
+// Strip strips a number of leading paths from file names on extraction.
 func (b *BinWrapper) Strip(value int) *BinWrapper {
 	b.strip = value
 	return b
 }
 
-//Adds command line argument to run the binary with.
+// Arg adds command line argument to run the binary with.
 func (b *BinWrapper) Arg(name string, values ...string) *BinWrapper {
 	values = append([]string{name}, values...)
 	b.args = append(b.args, values...)
 	return b
 }
 
-//Enabled debug output
+// Debug enables debug output
 func (b *BinWrapper) Debug() *BinWrapper {
 	b.debug = true
 	return b
 }
 
-//Returns arguments were added with Arg method
+// Args returns arguments were added with Arg method
 func (b *BinWrapper) Args() []string {
 	return b.args
 }
 
-//Returns the full path to the binary
+// Path returns the full path to the binary
 func (b *BinWrapper) Path() string {
 	src := osFilterObj(b.src)
 
@@ -149,42 +151,47 @@ func (b *BinWrapper) Path() string {
 
 	if b.dest == "." {
 		return b.dest + string(filepath.Separator) + b.execPath
-	} else {
-		return filepath.Join(b.dest, b.execPath)
 	}
 
+	return filepath.Join(b.dest, b.execPath)
 }
 
+// StdIn sets reader to read executable's stdin from
 func (b *BinWrapper) StdIn(reader io.Reader) *BinWrapper {
 	b.stdIn = reader
 	return b
 }
 
-//Returns the binary's standard output after Run was called
+// StdOut returns the binary's stdout after Run was called
 func (b *BinWrapper) StdOut() []byte {
 	return b.stdOut
 }
 
+// CombinedOutput returns combined executable's stdout and stderr
 func (b *BinWrapper) CombinedOutput() []byte {
 	return append(b.stdOut, b.stdErr...)
 }
 
+// SetStdOut set writer to write executable's stdout
 func (b *BinWrapper) SetStdOut(writer io.Writer) *BinWrapper {
 	b.stdOutWriter = writer
 	return b
 }
 
+// Env specifies the environment of the executable.
+// If Env is nil, Run uses the current process's environment.
+// Elements of env should be in form: "ENV_VARIABLE_NAME=value"
 func (b *BinWrapper) Env(env []string) *BinWrapper {
 	b.env = env
 	return b
 }
 
-//Returns the binary's standard error after Run was called
+// StdErr returns the executable's stderr after Run was called
 func (b *BinWrapper) StdErr() []byte {
 	return b.stdErr
 }
 
-//Removes all arguments set with Arg method, cleans StdOut and StdErr
+// Reset removes all arguments set with Arg method, cleans StdOut and StdErr
 func (b *BinWrapper) Reset() *BinWrapper {
 	b.args = []string{}
 	b.stdOut = nil
@@ -195,8 +202,8 @@ func (b *BinWrapper) Reset() *BinWrapper {
 	return b
 }
 
-//Runs the binary with provided arg list.
-//Arg list is appended to args set through Arg method
+// Run runs the binary with provided arg list.
+// Arg list is appended to args set through Arg method
 func (b *BinWrapper) Run(arg ...string) error {
 
 	if b.src != nil && len(b.src) > 0 {
@@ -266,7 +273,7 @@ func (b *BinWrapper) download() error {
 	src := osFilterObj(b.src)
 
 	if src == nil {
-		return errors.New("No binary found matching your system. It's probably not supported.")
+		return errors.New("No binary found matching your system. It's probably not supported")
 	}
 
 	file, err := b.downloadFile(src.url)
@@ -314,9 +321,9 @@ func (b *BinWrapper) extractFile(file string) error {
 
 	if b.strip == 0 {
 		return nil
-	} else {
-		return b.stripDir()
 	}
+
+	return b.stripDir()
 }
 
 func (b *BinWrapper) stripDir() error {
